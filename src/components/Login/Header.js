a2969eb3b7874e108f537f35b7c0acb681e85c50
srika <srikanthnayak583@gmail.com>
2021-06-13 09:36:17 -0400

    react assignment

--- src/components/Login/Header.js
+++ src/components/Login/Header.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { memo } from "react";
 
-const Header = () => {
+const Header = memo((props) => {
   return (
     <div className="login-container__header">
       <h2 className="login-container__header__title">alphametrica</h2>
       <p className="login-container__header__sub-title">로그인</p>
     </div>
   );
-};
+});
 
 export default Header;
